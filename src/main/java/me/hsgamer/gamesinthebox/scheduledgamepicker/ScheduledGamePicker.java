d6c64b21dcc26fc177dd280975587fc9c998abd2
HSGamer <huynhqtienvtag@gmail.com>
2023-03-23 21:28:11 +0700

    register & unregister the game picker

--- src/main/java/me/hsgamer/gamesinthebox/scheduledgamepicker/ScheduledGamePicker.java
+++ src/main/java/me/hsgamer/gamesinthebox/scheduledgamepicker/ScheduledGamePicker.java
@@ -1,10 +1,20 @@
 package me.hsgamer.gamesinthebox.scheduledgamepicker;
 
 import me.hsgamer.gamesinthebox.expansion.GameExpansion;
+import me.hsgamer.gamesinthebox.picker.GamePicker;
+import me.hsgamer.gamesinthebox.planner.Planner;
+import me.hsgamer.hscore.builder.Builder;
 
 public class ScheduledGamePicker implements GameExpansion {
+    private Builder.FunctionElement<Planner, GamePicker> element;
+
     @Override
     public void onEnable() {
-        getPlugin().getGamePickerManager().register(CronGamePicker::new, "cron");
+        element = getPlugin().getGamePickerManager().register(CronGamePicker::new, "cron");
+    }
+
+    @Override
+    public void onDisable() {
+        getPlugin().getGamePickerManager().remove(element);
     }
 }
